e0ae8fbf87f3dc58f43066de0828bde2cd4d43a4
Camilo Moncada <monk-da@hotmail.com>
2020-08-12 09:45:52 -0500

    advanced task 6

--- models/state.py
+++ models/state.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base, 
+from sqlalchemy import Column, Integer, String, ForeignKey
+from relationship_city import City
 
 
 class State(BaseModel):
     """ State class """
-    name = ""
+    __tablename__ = 'states'
+
+    name = Column(String(128), nulleable = False)
+    cities = relationship("City", backref="states", cascade="all, delete")
