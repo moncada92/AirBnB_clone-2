55914c062b05f6821bfb154d05f2c92e647d02c2
Luis Angel Vargas Mosquera <luisangelvargas91@gmail.com>
2020-08-13 11:57:36 -0500

    hotfix

--- models/user.py
+++ models/user.py
@@ -12,6 +12,6 @@ class User(BaseModel, Base):
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=True)
-    last_name = Column(String(129), nullable=True)
+    last_name = Column(String(128), nullable=True)
     places = relationship("Place", backref="user", cascade="all, delete")
     reviews = relationship("Review", backref="user", cascade="all, delete")
