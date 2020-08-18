c3e02f35e5a5e409e39701a4d3abee51e4d80a48
Camilo Moncada <monk-da@hotmail.com>
2020-08-17 21:37:14 -0500

    response question 0 deploy statics

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+#create folders and files 
+
+apt-get update
+apt-get -y install nginx
+ufw allow 'Nginx HTTP'
+
+mkdir -p /data
+mkdir -p /data/web_static/
+mkdir -p /data/web_static/releases/
+mkdir -p /data/web_static/shared/
+mkdir -p /data/web_static/releases/test/
+mkdir -p /data/web_static/releases/test/index.html
+ln -sf /data/web_static/current /data/web_static/releases/test/
+echo "<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>" > /data/web_static/releases/test/index.html
+chown -R ubuntu:ubuntu /data
+sed -i '/listen 80 default_server/a location /hbnb_static/ { alias /data/web_static/current/;}' /etc/nginx/sites-available/default
+service nginx restart
