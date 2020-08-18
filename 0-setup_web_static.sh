5249a0e7035c6edeea26a9307225f26109bd3f1f
Camilo Moncada <monk-da@hotmail.com>
2020-08-17 22:35:08 -0500

    hotfix question 0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -4,13 +4,11 @@
 apt-get update
 apt-get -y install nginx
 ufw allow 'Nginx HTTP'
-
 mkdir -p /data
 mkdir -p /data/web_static/
 mkdir -p /data/web_static/releases/
 mkdir -p /data/web_static/shared/
 mkdir -p /data/web_static/releases/test/
-mkdir -p /data/web_static/releases/test/index.html
 ln -sf /data/web_static/current /data/web_static/releases/test/
 echo "<html>
   <head>
@@ -20,5 +18,6 @@ echo "<html>
   </body>
 </html>" > /data/web_static/releases/test/index.html
 chown -R ubuntu:ubuntu /data
+chgrp -R ubuntu /data
 sed -i '/listen 80 default_server/a location /hbnb_static/ { alias /data/web_static/current/;}' /etc/nginx/sites-available/default
 service nginx restart
