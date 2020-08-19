8f565b9cfc3ff4259563bdda1c5cb714363ce3b0
Camilo Moncada <monk-da@hotmail.com>
2020-08-18 23:53:22 -0500

    hotfix 6 question 0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
 #create folders and files 
-
-apt-get update
-apt-get -y install nginx
-ufw allow 'Nginx HTTP'
-sudo mkdir -p /data/web_static/shared/
+sudo apt-get -y update
+sudo apt-get -y install nginx
+sudo ufw allow 'Nginx HTTP'
 sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared/
 echo "<html>
   <head>
   </head>
@@ -13,8 +12,7 @@ echo "<html>
     Holberton School
   </body>
 </html>" > /data/web_static/releases/test/index.html
-chown -R ubuntu:ubuntu /data
-chgrp -R ubuntu /data
-sed -i '/listen 80 default_server/a location /hbnb_static/ { alias /data/web_static/current/;}' /etc/nginx/sites-available/default
-service nginx restart
-exit 0
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo chown -R ubuntu:ubuntu /data/
+sudo sed -i '/listen 80 default_server/a location /hbnb_static/ { alias /data/web_static/current/;}' /etc/nginx/sites-available/default
+sudo service nginx restart
