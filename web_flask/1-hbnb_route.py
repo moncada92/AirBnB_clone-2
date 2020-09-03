2609fba676605014bad43ed36e273f4fc665d10b
Camilo Moncada <monk-da@hotmail.com>
2020-09-02 22:58:46 -0500

    hotfix 5 question 1

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,20 +1,20 @@
 #!/usr/bin/python3
-"""
-create second route
-"""
+"""start flask and set a route"""
 
 from flask import Flask
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def start_app():
+def hello():
+    """main route return"""
     return 'Hello HBNB!'
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    return 'HBNB!'
+    """return in other route"""
+    return 'HBNB'
 
 
 if __name__ == '__main__':
