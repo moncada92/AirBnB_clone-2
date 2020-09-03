cc910a450d1298f9bd7f854a8470b6d6b6a6583d
Camilo Moncada <monk-da@hotmail.com>
2020-09-02 22:50:53 -0500

    hotfix 2 question 0

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,15 +1,13 @@
-#!usr/bin/python3
+#!/usr/bin/python3
 """
 create first route
 """
 
 from flask import Flask
-
 app = Flask(__name__)
-app.url_map.strict_slashes = False
 
 
-@app.route('/')
+@app.route('/', strict_slashes=False)
 def start_app():
     return 'Hello HBNB!'
 
