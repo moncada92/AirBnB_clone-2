a46189753089849eba6bcb37efc8da0f5d0aeb63
Camilo Moncada <monk-da@hotmail.com>
2020-09-02 23:00:18 -0500

    hotfix  question 2

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,27 +1,26 @@
-#!usr/bin/env python3
+#!/usr/bin/python3
 """
 create second route with variables
 """
 
 from flask import Flask, escape
-
 app = Flask(__name__)
-app.url_map.strict_slashes = False
 
 
-@app.route('/')
+@app.route('/', strict_slashes=False)
 def start_app():
     return 'Hello HBNB!'
 
 
-@app.route('/hbnb')
+@app.route('/hbnb', strict_slashes=False)
 def hbnb():
     return 'HBNB!'
 
 
-@app.route('/c/<string:var>')
+@app.route('/c/<string:var>', strict_slashes=False)
 def path(var):
-    return 'C %s' % escape(var.replace('_',' '))
+    return 'C %s' % escape(var.replace('_', ' '))
+
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port="5000")
