aad519e866a44ff799e009e8f24fbb5c50f338b0
Camilo Moncada <monk-da@hotmail.com>
2020-09-02 22:43:10 -0500

    hotfix question 0

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,4 +1,4 @@
-#!usr/bin/env python3
+#!usr/bin/python3
 """
 create first route
 """
@@ -12,6 +12,7 @@ app.url_map.strict_slashes = False
 @app.route('/')
 def start_app():
     return 'Hello HBNB!'
+
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port="5000")
