5ce3123eaa79fbf2854f55e7e71c5c71aa914b69
Camilo Moncada <monk-da@hotmail.com>
2020-09-03 23:39:34 -0500

    response question 12

--- web_flask/100-hbnb.py
+++ web_flask/100-hbnb.py
@@ -7,7 +7,8 @@ from flask import Flask, escape, render_template
 from models import storage
 from models.state import State
 from models.amenity import Amenity
-
+from models.place import Place
+from models.user import User
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
@@ -19,12 +20,18 @@ def close(self):
     storage.close()
 
 
-@app.route('/hbnb_filters', strict_slashes=False)
+@app.route('/hbnb', strict_slashes=False)
 def listStateId():
+    places = storage.all(Place).values()
     states = storage.all(State).values()
     amenities = storage.all(Amenity).values()
+    result = {}
 
-    return render_template('10-hbnb_filters.html', **locals())
+    for p, u in storage._DBStorage__session.query(Place, User).\
+            filter(Place.user_id == User.id):
+        result[p.user_id] = "{} {}".format(u.first_name, u.last_name)
+
+    return render_template('100-hbnb.html', **locals())
 
 
 if __name__ == '__main__':
